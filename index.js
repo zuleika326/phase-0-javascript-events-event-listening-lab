fb93f10d79639768c3c16b10220cb76fb9e8867c
zuleika <njokizuleika@gmail.com>
2023-03-03 15:04:19 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
 }
